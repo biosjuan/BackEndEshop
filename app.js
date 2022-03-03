f8d0e1fcc0b2815be8a4d35f77d34728d0c0945e
Lee <hyuk.lee@accenture.com>
2022-03-03 08:53:13 -0800

    added routes and models

--- app.js
+++ app.js
@@ -2,19 +2,29 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 require('dotenv/config');
-const api = process.env.API_URL;
 
-
-const productRouter = require('./routers/products');
+app.use(cors());
+app.options('*', cors());
 
 // Middleware
 app.use(express.json());
 app.use(morgan('tiny'));
 
-// Routers
-app.use(`${api}/products`, productRouter);
+
+// Routers 
+const categoriesRoutes = require('./routes/categories');
+const productsRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
+
+const api = process.env.API_URL;
+
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
 
 
 mongoose.connect(process.env.CONNECTION_STRING).then(() => {
